16ac2f8cd832b573c8410facc8c97534de0173b5
dei2r <davidpastorberenguer@gmail.com>
2025-11-19 20:51:33 +0100

    ADD added more values to PlaylistFactory

--- backend/laravel/database/factories/PlaylistFactory.php
+++ backend/laravel/database/factories/PlaylistFactory.php
@@ -8,7 +8,28 @@ class PlaylistFactory extends Factory {
     
     public function definition() {
 
-        $temas = ['Top Urbano', 'Fiesta', 'Relax', 'Favoritas', 'Noche de Flow', 'Playlist del Dei'];
+        $temas = [
+            'Urban Vibes España',
+            'Reggaeton Hits PR',
+            'Flow Latino',
+            'Trap y Beats',
+            'Ritmo Callejero',
+            'Top Urbano 2025',
+            'Canciones del Barrio',
+            'Latino Party',
+            'Urbano y Chill',
+            'Fiesta Boricua',
+            'Éxitos Callejeros',
+            'Beats de Medianoche',
+            'Reggaeton Forever',
+            'Sonidos del Caribe',
+            'Urban Legends',
+            'Trap del Momento',
+            'Ritmo de Calle',
+            'Vibra Latina',
+            'Urbano y Remix',
+            'Street Beats'
+        ];
 
         return [
             'name' => $this->faker->unique()->randomElement($temas),
